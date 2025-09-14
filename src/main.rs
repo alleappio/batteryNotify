9e39c596b8a1028ea3d1153f90ca862de79f7a9b
alleappio <allevanny@gmail.com>
2025-09-14 23:18:03 +0200

    add get battery level function
    Signed-off-by: alleappio <allevanny@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::process::Command;
+use regex::Regex;
 
-fn send_notification(level: i8){
+fn send_notification(level: u32){
     let level_string = format!("Battery level at {}%", level);
 
     Command::new("notify-send")
@@ -14,6 +15,30 @@ fn send_notification(level: i8){
         .expect("Failed to execute program");
 }
 
-fn main(){
-    send_notification(20);
+fn get_battery_level(battery: &str) -> Option<u32>{
+    let battery_string = format!("/org/freedesktop/UPower/devices/battery_{}", battery);
+    let command = Command::new("upower")
+                        .arg("-i")
+                        .arg(battery_string)
+                        .output()
+                        .expect("Failed to execute program");
+
+    let total_string = match String::from_utf8(command.stdout){
+        Ok(v) => v,
+        Err(e) => panic!("aiutooooooo"),
+    };
+
+    let re = Regex::new(r"percentage:\s+(\d+)%").unwrap();
+    if let Some(results) = re.captures(&total_string) {
+        results[1].parse::<u32>().ok()
+    }else{
+        None
+    }
 }
+
+fn main(){
+    let battery_level = get_battery_level("BAT1").unwrap();
+    send_notification(battery_level);
+}
+
+
