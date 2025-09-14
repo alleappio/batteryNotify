134fc3dbed529a63575d1a04416a614b8990e954
alleappio <allevanny@gmail.com>
2025-09-14 22:15:37 +0200

    add send notification function
    Signed-off-by: alleappio <allevanny@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::process::Command;
+
+fn send_notification(level: i8){
+    let level_string = format!("Battery level at {}%", level);
+
+    Command::new("notify-send")
+        .arg("-u")
+        .arg("critical")
+        .arg("-t")
+        .arg("10000")
+        .arg("LOW BATTERY")
+        .arg(level_string)
+        .spawn()
+        .expect("Failed to execute program");
+}
+
 fn main(){
-    println!("hello world");
+    send_notification(20);
 }
